2235f17259fa7782c144f74d766912a1e1dd4c53
tatenami <ttnm139th@gmail.com>
2023-12-23 14:40:56 +0900

    correct args

--- socket_unit.cpp
+++ socket_unit.cpp
@@ -1,6 +1,9 @@
 #include "socket_unit.h"
 
 const string brd_addr = "255.255.255.255";
+const string loopback_addr = "127.0.0.1";
+
+/*-----------[destionation device data]------------*/
 
 DstData::DstData(string ip_addr, int port){
     addr.sin_family = AF_INET;
@@ -8,8 +11,11 @@ DstData::DstData(string ip_addr, int port){
     addr.sin_port = htons(port);
 }
 
-void SocketUnit::bind_socket(){
-    bind(global_socket.sock, (const struct sockaddr *)&(global_socket.addr), global_socket.addr_len);
+/*-----------[socket unit function]------------*/
+
+void SocketUnit::bind_socket(socket_data& sd){
+    bind(sd.sock, (const struct sockaddr *)&(sd.addr), sd.addr_len);
+    //bind(global_socket.sock, (const struct sockaddr *)&(global_socket.addr), global_socket.addr_len);
 }
 
 SocketUnit::~SocketUnit(){
@@ -34,14 +40,35 @@ int SocketUnit::get_src_port(){
     return ntohs(src_data.sin_port);
 }
 
+/*-----------[UDP unit function]-----------*/
+
+void UDPUnit::set_socket(socket_data& sd, string ip_addr, uint16_t port){
+    sd.sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    sd.addr.sin_family = AF_INET;
+    sd.addr.sin_addr.s_addr = inet_addr(ip_addr.c_str());
+    sd.addr.sin_port = htons(port);
+    sd.addr_len = sizeof(sd.addr);
+}
 
 UDPUnit::UDPUnit(string ip_addr, int port){
-    global_socket.sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    global_socket.addr.sin_family = AF_INET;
-    global_socket.addr.sin_addr.s_addr = inet_addr(ip_addr.c_str());
-    global_socket.addr.sin_port = htons(port);
-    global_socket.addr_len = sizeof(global_socket.addr);
-    bind_socket();
+    // global_socket.sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    // global_socket.addr.sin_family = AF_INET;
+    // global_socket.addr.sin_addr.s_addr = inet_addr(ip_addr.c_str());
+    // global_socket.addr.sin_port = htons(port);
+    // global_socket.addr_len = sizeof(global_socket.addr);
+    set_socket(global_socket, ip_addr, port);
+    bind_socket(global_socket);
+}
+
+int UDPUnit::set_local_socket(uint16_t local_port){
+    // local_socket.sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    // local_socket.addr.sin_family = AF_INET;
+    // local_socket.addr.sin_addr.s_addr = inet_addr(loopback_addr.c_str());
+    // local_socket.addr.sin_port = htons(local_port);
+    // local_socket.addr_len = sizeof(local_socket.addr);
+    set_socket(local_socket, loopback_addr, local_port);
+    bind_socket(local_socket);
+    return 0;
 }
 
 int UDPUnit::send(void *buf, DstData *dst){
--- socket_unit.h
+++ socket_unit.h
@@ -19,6 +19,8 @@ using namespace std;
 extern const string brd_addr;
 const static int broadcast_flag = 1;
 
+/*----------[socket data struct]-----------*/
+
 typedef struct{
     int sock;
     struct sockaddr_in addr;
@@ -31,12 +33,19 @@ public:
     DstData(string ip_addr, int port);
 };
 
+/*----------[socket_unit: base class]----------*/
+
 class SocketUnit{
 protected:
     socket_data global_socket;
+    socket_data local_socket;
+
     struct sockaddr_in src_data;
+    bool opened_local_socket = false;
     uint8_t RxBuf[BUF_SIZE];
-    void bind_socket();
+
+    virtual void set_socket(socket_data& sd, string ip_addr, uint16_t port) = 0;
+    void bind_socket(socket_data& sd);
 public:
     ~SocketUnit();
 
@@ -46,15 +55,21 @@ public:
     int get_src_port();
 };
 
+/*----------[UDP_socket_unit]-----------*/
+
 class UDPUnit: public SocketUnit{
 protected:
     socket_data bc_data;
     bool permission_broadcast = false;
+
+    void set_socket(socket_data& sd, string ip_addr, uint16_t port);         
 public:
     UDPUnit(string ip_addr, int port);
-    int send(void *buf, DstData *dst);
+    int set_local_socket(uint16_t local_port);
 
+    int send(void *buf, DstData *dst);
     bool enable_broadcast(int bc_port);
     int send_all(void *buf);
 };
+
 #endif
